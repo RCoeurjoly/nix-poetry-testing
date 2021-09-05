cf5a6833a513c97ad47bb8ea7f09847126383358
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-09-05 16:38:57 +0200

    WIP

--- script.sh
+++ script.sh
@@ -1,28 +1,42 @@
-for quoted_package in $(cat packages.json | jq .rows[].project)
-do
-    unquoted_package=${quoted_package//\"}
-    git rev-parse --quiet --verify $unquoted_package
-    rc=$?
-    if [[ $rc = 0 ]]; then
-        echo Package $unquoted_package is already handled
-    else
-        git checkout -b $unquoted_package
-        git push origin --delete $unquoted_package
-        poetry add $unquoted_package
+check_branches () {
+    for branch in $(git branch --no-color | grep -v "main")
+    do
+        git show --pretty="" --name-only $branch | grep -q uninstallable
         rc=$?
-        if [[ $rc != 0 ]]; then
-            echo Package $unquoted_package failed to install with poetry
-            echo $unquoted_package >> uninstallable_packages
-            git checkout -- poetry.lock
-            git add uninstallable_packages
-            git commit -m \"Uninstallable $unquoted_package\"
-            git checkout main
-            git merge $unquoted_package
-        else
-            git add --all
-            git commit -m $unquoted_package
-            git push
+        if [[ $rc = 0 ]]; then
+            echo $branch
         fi
-        git checkout main
-    fi
-done
+    done
+}
+
+test_packages () {
+    for quoted_package in $(cat packages_small.json | jq .rows[].project)
+    do
+        unquoted_package=${quoted_package//\"}
+        git rev-parse --quiet --verify $unquoted_package
+        rc=$?
+        if [[ $rc = 0 ]]; then
+            echo Package $unquoted_package is already handled
+        else
+            git checkout -b $unquoted_package
+            git push origin --delete $unquoted_package
+            poetry add $unquoted_package
+            rc=$?
+            if [[ $rc != 0 ]]; then
+                echo Package $unquoted_package failed to install with poetry
+                echo $unquoted_package >> uninstallable_packages
+                git checkout -- poetry.lock
+                git add uninstallable_packages
+                git commit -m \"Uninstallable $unquoted_package\"
+                git checkout main
+                git merge $unquoted_package
+                git branch -d $unquoted_package
+            else
+                git add --all
+                git commit -m $unquoted_package
+                git push
+            fi
+            git checkout main
+        fi
+    done
+}
