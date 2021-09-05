0def427de4e1dacd7adc3f9426795d32eefb8e87
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-09-05 14:42:34 +0200

    WIP

--- script.sh
+++ script.sh
@@ -1,12 +1,16 @@
 for quoted_package in $(cat packages.json | jq .rows[].project)
 do
     unquoted_package=${quoted_package//\"}
-    git branch -D $unquoted_package
-    git push origin --delete $unquoted_package
-    git checkout -b $unquoted_package
-    poetry add $unquoted_package
-    git add --all
-    git commit -m $unquoted_package
-    git push
-    git checkout main
+    git rev-parse --quiet --verify $unquoted_package
+    rc=$?
+    if [[ $rc = 0 ]]; then
+        Package is already handled
+    else
+        git checkout -b $unquoted_package
+        poetry add $unquoted_package
+        git add --all
+        git commit -m $unquoted_package
+        git push
+        git checkout main
+    fi
 done
