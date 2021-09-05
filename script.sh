189043563f5907e64984ea79d159ebf2b9f6fb5a
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-09-05 15:00:37 +0200

    Add uninstallable package to list

--- script.sh
+++ script.sh
@@ -4,13 +4,23 @@ do
     git rev-parse --quiet --verify $unquoted_package
     rc=$?
     if [[ $rc = 0 ]]; then
-        Package is already handled
+        echo Package $unquoted_package is already handled
     else
         git checkout -b $unquoted_package
         poetry add $unquoted_package
-        git add --all
-        git commit -m $unquoted_package
-        git push
+        rc=$?
+        if [[ $rc != 0 ]]; then
+            echo Package $unquoted_package failed to install with poetry
+            echo $unquoted_package >> uninstallable_packages
+            git add --all
+            git commit -m \"Uninstallable $unquoted_package\"
+            git checkout main
+            git merge $unquoted_package
+        else
+            git add --all
+            git commit -m $unquoted_package
+            git push
+        fi
         git checkout main
     fi
 done
