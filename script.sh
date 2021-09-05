437582e82855fb4eee9178925d9fcdce2a91b9ce
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-09-05 14:19:20 +0200

    Delete remote branch before pushing

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ for quoted_package in $(cat packages.json | jq .rows[].project)
 do
     unquoted_package=${quoted_package//\"}
     git branch -D $unquoted_package
+    git push origin --delete $unquoted_package
     git checkout -b $unquoted_package
     poetry add $unquoted_package
     git add --all
